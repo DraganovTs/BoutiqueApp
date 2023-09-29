f7da95d2c12927bb534dfe6103ecc67a8fe494f5
DraganovTs <cvetomir_draganov@abv.bg>
2023-09-29 10:52:28 +0300

    init

---
+++ src/main/java/com/homecode/BoutiqueApp/BoutiqueAppApplication.java
@@ -0,0 +1,13 @@
+package com.homecode.BoutiqueApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueAppApplication.class, args);
+	}
+
+}
