2d3879a82808151d813798db0e35376ffcae9797
DraganovTs <cvetomir_draganov@abv.bg>
2023-10-05 21:17:24 +0300

    Resilience4j integrated

---
+++ api-gateway/src/main/java/com/homecode/controller/FallbackController.java
@@ -0,0 +1,24 @@
+package com.homecode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback/customers")
+    public String customerServiceFallback() {
+        System.out.println("Fallback invoked for customer service");
+        return "Fallback response for customer service";
+    }
+
+    @GetMapping("/fallback/orders")
+    public String orderServiceFallback() {
+        return "Fallback response for order service";
+    }
+
+    @GetMapping("/fallback/products")
+    public String productServiceFallback() {
+        return "Fallback response for product service";
+    }
+}
