1587d20eb9b5e35c25fb785818b1ae8b5695b49a
DraganovTs <cvetomir_draganov@abv.bg>
2023-10-21 11:04:05 +0300

    docker

--- commons/src/main/java/com/homecode/commons/dto/CategoryDTO.java
+++ commons/src/main/java/com/homecode/commons/dto/CategoryDTO.java
@@ -6,8 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,11 +16,8 @@ import java.util.Set;
 public class CategoryDTO {
 
     private Long id;
-    @NotNull
-    @Size(max = 50)
     private String name;
-    @NotNull
-    @Size(max = 300)
+
     private String description;
 
     private Set<ProductDTO> products = new HashSet<>();
