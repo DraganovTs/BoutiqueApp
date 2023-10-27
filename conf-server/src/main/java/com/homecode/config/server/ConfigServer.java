9f54d509bd0e9c171607432378a198b0ca891212
DraganovTs <cvetomir_draganov@abv.bg>
2023-10-27 20:20:34 +0300

    transform to container microservices

--- conf-server/src/main/java/com/homecode/config/server/ConfigServer.java
+++ conf-server/src/main/java/com/homecode/config/server/ConfigServer.java
@@ -1,4 +1,4 @@
-package com.homecode.config;
+package com.homecode.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServer {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class,args);
+        SpringApplication.run(ConfigServer.class, args);
     }
 }
